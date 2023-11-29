852b45db346315ffd959353f52dd931fc824ad26
LaylaR <layla.richard.2111@gmail.com>
2023-11-29 11:03:25 +0100

    access dom through hostBinding decorator

--- src/app/directives/basic-directive.directive.ts
+++ src/app/directives/basic-directive.directive.ts
@@ -1,13 +1,14 @@
-import {Directive, ElementRef, HostListener, OnInit, Renderer2} from '@angular/core';
+import {Directive, ElementRef, HostBinding, HostListener, OnInit, Renderer2} from '@angular/core';
 
 @Directive({
   selector: '[appBasicDirective]'
 })
 export class BasicDirectiveDirective implements OnInit {
-//need to access the element on which the directive is applied
-  // available through injection
-  //carefull, angular can be run without a browser, so no DOM in this case. Acessing directly the DOM is then not a best practise
-  //Should be access through the renderer to avoid error in case of no DOM (not frequent but still)
+  //need to access the element on which the directive is applied
+  // available through injection in the ctor
+  //carefull, angular can be run without a browser, so no DOM in this case. Accessing directly the DOM is then not a best practise
+  //Should be access through the renderer to avoid error in case of no DOM (not frequent but still) => also injected through the ctor
+  //second way to access the dom safely without using rendered is decorator @HostBinding
   constructor(private elemRef:ElementRef, private renderer:Renderer2) {
 
   }
@@ -17,13 +18,19 @@ export class BasicDirectiveDirective implements OnInit {
     // this.renderer.setStyle(this.elemRef.nativeElement,'background-color','lightblue');
   }
 
+  //Second option to access dom and property on the element on which the directive is applied
+  @HostBinding('style.color') color:string='black';
   @HostListener('mouseenter') onHoover(){
-    this.renderer.setStyle(this.elemRef.nativeElement,'background-color','lightblue');
+    this.renderer.setStyle(this.elemRef.nativeElement,'background-color','blue');
+    this.color='white';
   }
 
   @HostListener('mouseleave') onLeave(){
     this.renderer.setStyle(this.elemRef.nativeElement,'background-color','transparent');
+    this.color='black';
   }
+
+
 
 
 
